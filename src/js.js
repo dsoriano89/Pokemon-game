7101db40dffa235613a635e7d7d38c7740f16de2
Deborah Soriano <dsoriano4389@gmail.com>
2023-05-28 02:31:09 -0300

    change theme
    dark/light
    gifs

--- src/js.js
+++ src/js.js
@@ -1,13 +1,4 @@
 const changeTheme = document.getElementById("changeTheme");
-const main = document.querySelector("main");
-const changeImg = document.querySelector("changeImg")
-changeTheme.addEventListener("click", () => {
-    const darkThemeActive = main.classList.contains("darkTheme");
-if (darkThemeActive) {
-    main.classList.remove("darkTheme")
-}
-    main.classList.add("darkTheme")
-    changeImg.setAttribute("src", "./src/imgs/moon.png")
+changeTheme.addEventListener("change", function () {
+    document.body.classList.toggle("dark")
 })
-
-
